2b69a3c7fc9421c569538e61846c751f06e661ef
jcssilva <jcss.silva@hotmail.com>
2021-03-14 17:47:25 -0300

    Add project files.

---
+++ Netcore+TS/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Netcore_TS
+{
+    public class Startup
+    {
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
